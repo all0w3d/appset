ab568943c390e51c0d25c1f4ad74d244a66f875e
IvanRylkov <all0w3d@gmail.com>
2021-06-13 17:42:08 +0300

    first

---
+++ src/js/main.js
@@ -0,0 +1,13 @@
+import "../scss/main.scss";
+import "../index.html";
+import "./modules/header";
+import "./modules/counter";
+import "./modules/modal";
+import "./modules/burger";
+import "./modules/slider";
+
+
+
+
+
+
